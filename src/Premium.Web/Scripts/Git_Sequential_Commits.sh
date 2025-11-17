4efe7c806b3be75c2607db93d69a541a6f424ba3
narasaiahmannam2022 <narasaiahmannam2022@gmail.com>
2025-11-17 17:45:21 +0530

    Added Git Sequential Commits Script

---
+++ src/Premium.Web/Scripts/Git_Sequential_Commits.sh
@@ -0,0 +1,29 @@
+
+
+git init
+git add README.md
+git commit -m "Initial commit with project summary"
+
+git add Models/
+git commit -m "Add view models and request models"
+
+git add Services/
+git commit -m "Add premium calculation service + factor provider"
+
+git add Controllers/
+git commit -m "Add MVC and API controllers"
+
+git add Views/
+git commit -m "Add UI pages and JS script"
+
+git add wwwroot/
+git commit -m "Add CSS and JS static assets"
+
+git add tests/
+git commit -m "Add unit tests for premium calculation"
+
+git add docs/
+git commit -m "Add database design document"
+
+git push origin main
+
